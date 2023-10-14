a8874979ecafb3c7537b1abc043e3097f26668cb
ArtiomKochkin <artiom.kochkin@yandex.ru>
2023-10-14 17:12:02 +0700

    add a dynamism to the menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import * as functions from "./modules/functions.js";
 import * as menu from "./components/menu.js";
+import * as menuContent from "./components/menuContent.js";
 
 functions.isWebp();
 menu.toggleMenu();
+menuContent.menuContent();
