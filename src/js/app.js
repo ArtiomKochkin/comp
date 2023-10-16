bb41e0dc51124dea5a7088158c2ad9a37ab2908b
ArtiomKochkin <artiom.kochkin@yandex.ru>
2023-10-16 21:18:50 +0700

    fix bugs in header

--- src/js/app.js
+++ src/js/app.js
@@ -4,12 +4,10 @@ import * as menuContent from "./components/menuContent.js";
 import * as catalog from "./components/catalog.js";
 import * as modalGeneral from "./components/modalGeneral.js";
 import * as comparisonHeader from "./components/comparisonHeader.js";
-import * as header from "./components/header.js";
 
 functions.isWebp();
 menu.toggleMenu();
 menuContent.menuContent();
-header.scrollHeader();
 comparisonHeader.toggleComparisonList();
 modalGeneral.toggleModalGeneral();
 catalog.toggleCatalog();
