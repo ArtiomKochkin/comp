e91b08b446f1ba678e9c98ff50ae9774d7ceb29c
ArtiomKochkin <artiom.kochkin@yandex.ru>
2023-10-25 15:27:50 +0700

    Fix: change map

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,6 @@ import * as menuContent from "./components/menuContent.js";
 import * as catalog from "./components/catalog.js";
 import * as modalGeneral from "./components/modalGeneral.js";
 import * as comparisonHeader from "./components/comparisonHeader.js";
-import * as contacts from "./components/contacts.js";
 import * as sliderPromotion from "./components/sliderPromotion.js";
 import * as sliderIntro from "./components/sliderIntro.js";
 import * as sliderReviews from "./components/sliderReviews.js";
@@ -23,9 +22,6 @@ switch (window.location.pathname) {
     case "/index.html" : {   
         sliderIntro.initSlider();
         sliderReviews.initSlider();
-    } break;
-    case "/contacts.html" : {
-        contacts.map();
     } break;
     case "/promotion.html" : {
         sliderPromotion.initSlider();
